3872481a977b0da9e2a72ce26f48e5dd25f6e429
anjana varma <anjana.varma@digio.com.au>
2023-03-21 12:16:09 +1100

    fix: bug fix

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 "log"
+"time"
 )
 
 func dummy(c chan int) {
+time.Sleep(3 * time.Second)
 random := <-c
 log.Println("receiving--", random)
 }
 
 func main(){
 ch := make(chan int)
-go dummy(ch);
+go dummy(ch)
 log.Println("sending--")
-ch <- 100;
+ch <- 100
 }
