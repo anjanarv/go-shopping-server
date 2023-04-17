a567efdb93f436969eb86b9dd2278233ce727c74
anjana varma <anjana.varma@digio.com.au>
2023-04-17 13:14:23 +1000

    t

--- .github/workflows/tag.sh
+++ .github/workflows/tag.sh
@@ -1,32 +1,32 @@
-#! /bin/bash
-
-# Assumes that you tag versions with the version number (e.g., "1.1")
-# and then the build number is that plus the number of commits since
-# the tag (e.g., "1.1.17")
-
-DESCRIBE=$(git describe --tags `git rev-list --tags --max-count=1`)
-
-echo "first="$DESCRIBE
-
-# increment the build number (ie 115 to 116)
-MAJOR=`echo $DESCRIBE | awk '{split($0,a,"."); print a[1]}'`
-MINOR=`echo $DESCRIBE | awk '{split($0,a,"."); print a[2]}'`
-PATCH=`echo $DESCRIBE | awk '{split($0,a,"."); print a[3]}'`
-
-logmsg=$(git log -1 --pretty=oneline ${hash} | awk '$0~var {print $2}' var="${hash}")
-
-echo $logmsg
-
-MAJOR=${MAJOR:1}
-echo "MAJOR VERSION="$MAJOR
-
-NEWVERSION=""
-case "$logmsg" in
-    "fix:") NEWVERSION=$MAJOR:$MINOR:$(($PATCH+1));;
-    "feat:") NEWVERSION=$MAJOR:$(($MINOR+1)):$PATCH;;
-    "BREAKING CHANGE:") NEWVERSION=v$(($MAJOR+1)):$MINOR:$PATCH;;
-    "*") NEWVERSION=$MAJOR:$((MINOR+1)):$PATCH;;
-esac
-
-echo $NEWVERSION
-export NEWTAG=$NEWVERSION
+#!/bin/bash
+#CHANGED_PKG=$1
+#if [ -z $1 ]; then
+#    indent 4 "- [${RED}ERROR${RESET}] - Environment variable CHANGED_PKG is not defined or empty."
+#    echo ""
+#    exit 1
+#fi
+echo "TAGGING CHANGED PKG - "$CHANGED_PKG
+DESCRIBE=$(git describe --match "sampleserver/v*" --tags `git rev-list --tags --max-count=1`) >/dev/null
+NEWTAG=""
+if [ -z "$DESCRIBE" ]; then
+    echo "No previous tags exist in format ${CHANGED_PKG}/vx.x.x-rc*.Creating new version tag v1.0.0-rc1 ..."
+    NEWTAG="v1.0.0-rc1"
+else
+    echo "Previous tags exist in format ${CHANGED_PKG}/vx.x.x"
+    MAJOR=`echo $DESCRIBE | awk '{split($0,a,"."); print a[1]}'`
+    MINOR=`echo $DESCRIBE | awk '{split($0,a,"."); print a[2]}'`
+    PATCH=`echo $DESCRIBE | awk '{split($0,a,"."); print a[3]}'`
+    PATCH=`echo $PATCH | awk '{split($0,a,"-"); print a[1]}'`
+if [[ $DESCRIBE == *rc[0-9]* ]]; then
+    echo "RC tags exist in format ${CHANGED_PKG}/vx.x.x-rc*"
+    RC=`echo $DESCRIBE | awk '{split($0,a,"-"); print a[2]}' | tr -dc '0-9'`
+  NEWTAG=$MAJOR.$MINOR.$PATCH-$(($RC+1));
+else
+  NEWTAG=$MAJOR.$(($MINOR+1)).$PATCH-rc1;
+fi
+fi
+NEWTAG=$CHANGED_PKG/$NEWTAG
+echo "New tag created: "$NEWTAG
+tag=$(git tag -a $NEWTAG -m $NEWTAG)
+push_tag=$(git push --tags)
+echo "New tag successfully pushed to remote "$push_tag
